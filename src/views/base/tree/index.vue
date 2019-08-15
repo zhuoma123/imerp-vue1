e556c1e341564225666e6c995b7ce2634b0f8975
matthewleee <mattewlee@foxmail.com>
2019-08-15 18:49:39 +0800

    封装下拉框

--- src/views/base/tree/index.vue
+++ src/views/base/tree/index.vue
@@ -43,7 +43,7 @@
             </el-form-item>
         </el-form>
         <!--data show-->
-        <d2-crud
+        <!--<d2-crud
                 :columns="columns"
                 :options="options"
                 selectionRow
@@ -54,7 +54,29 @@
                 @sort-change="dataListSortChangeHandle"
                 @user-update="addOrUpdateHandleSetter"
                 @user-delete="deleteHandleSetter"
-        ></d2-crud>
+                :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+        ></d2-crud>-->
+        <el-card>
+            <!-- 查询结果 -->
+            <el-table v-loading="dataListLoading" :data="dataList" row-key="id" element-loading-text="正在查询中。。。"
+                      border fit highlight-current-row>
+                <el-table-column label="序号" type="index" align="center" show-overflow-tooltip width="50px"/>
+                <el-table-column align="center" label="类型" prop="type"/>
+                <el-table-column align="center" label="编码" prop="code"/>
+                <el-table-column align="center" label="名称" prop="name"/>
+                <el-table-column align="center" label="排序序号" prop="orderNum"/>
+                <el-table-column align="center" label="是否系统数据" prop="sys"/>
+                <el-table-column align="center" label="备注" prop="remark"/>
+                <el-table-column align="center" label="更新人" prop="orderNum"/>
+                <el-table-column align="center" label="更新时间" prop="orderNum"/>
+                <el-table-column align="center" label="操作" class-name="small-padding" width="160px">
+                    <template slot-scope="scope">
+                        <el-button type="primary" size="mini" @click="add(scope.row)">编辑</el-button>
+                        <el-button type="danger" size="mini" @click="del(scope.row)">删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-card>
         <!-- 弹窗, 新增 / 修改 -->
         <AddOrUpdate v-if="addOrUpdateVisible" :parentDataList="dataList" ref="addOrUpdate" @refreshDataList="getDataList"/>
     </d2-container>
@@ -115,7 +137,18 @@ export default {
   components: {
     AddOrUpdate
   },
-  methods: {},
+  methods: {
+    add (row) {
+      let map = {}
+      map.row = row
+      this.addOrUpdateHandleSetter(map)
+    },
+    del (row) {
+      let map = {}
+      map.row = row
+      this.deleteHandleSetter(map)
+    }
+  },
   created () {
     this.getDataList()
   }

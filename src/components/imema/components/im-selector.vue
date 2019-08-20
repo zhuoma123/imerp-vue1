98509a3da026876a7b8886bfa927195e8b2de086
韦庆武 <471038423@qq.com>
2019-08-20 13:29:46 +0800

    下拉选择器

---
+++ src/components/imema/components/im-selector.vue
@@ -0,0 +1,141 @@
+<template>
+  <el-select
+    :value="selectVal"
+    :filterable="selType == 'dyamic'"
+    :placeholder="placeholder"
+    :remote="selType == 'dyamic'"
+    :automatic-dropdown="selType == 'dyamic'"
+    default-first-option
+    :loading="loading"
+    :remote-method="_selDyamicList"
+    @change="e => _selChange(e)">
+    <el-option
+      v-for="item in options"
+      :key="item.key"
+      :label="item.key"
+      :value="item">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+// dataType对应的映射，用于生成对应的服务器访问地址
+const TYPE_MAP = {
+  customer: {
+    text: '请选择客户',
+    url: 'biz/customer',
+    type: 'dyamic'
+  },
+  code: {
+    text: '请选择',
+    url: 'code/',
+    type: 'static'
+  },
+  dict: {
+    text: '请选择',
+    url: 'dict/',
+    type: 'static'
+  }
+}
+
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    // 组件业务类型
+    dataType: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      dataForm: {
+        // customerId: 1
+      },
+      selectVal: this.value,
+      loading: false,
+      placeholder: '',
+      options: [],
+      codeType: '',
+      dType: '',
+      selType: 'static',
+      lock: false
+    }
+  },
+  created () {
+    if (this.dataType.includes('.')) {
+      this.dType = this.dataType.split('.')[0]
+      this.codeType = this.dataType.split('.')[1]
+    } else {
+      this.dType = this.dataType
+    }
+    this.selType = TYPE_MAP[this.dType].type
+    if (this.selType === 'static') {
+      this._selLoadCode()
+    }
+    this.placeholder = TYPE_MAP[this.dType].text
+  },
+  mounted () {
+
+  },
+  methods: {
+    _selLoadCode () {
+      this.lock = true
+      this._selDyamicList('').then(() => {
+        this.lock = false
+      })
+    },
+    async _selDyamicList (query) {
+      this.loading = true
+      await this.$axios.post(
+        '/common/' + TYPE_MAP[this.dType].url.replace(/^\//, '') + this.codeType,
+        { query: query }
+      ).then(res => {
+        this.loading = false
+        if (res && res.length > 0) {
+          this.options = res
+        } else { this.options = [] }
+      })
+    },
+    _selChange (e) {
+      this.selectVal = e
+      this.$emit('change', e)
+    },
+    _setVal (e) {
+      if (e.value && this.selType === 'dyamic') {
+        this.options = [e]
+      }
+      if (e.value && this.selType === 'static') {
+        let timer = setInterval(() => {
+          if (this.lock) {
+            return
+          }
+          this.options.map(item => {
+            if (this._.toString(e.value) === this._.toString(item.value)) { this.selectVal = item }
+          })
+          clearInterval(timer)
+        }, 500)
+      } else { this.selectVal = e }
+    }
+  },
+  watch: {
+    // 判断下拉框的值是否有改变
+    value (val) {
+      this.selectVal = val // ②监听外部对props属性result的变更，并同步到组件内的data属性myResult中
+    },
+    // ③组件内对myResult变更后向外部发送事件通知
+    selectVal (val, oldVal) {
+      this.$emit('input', val)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.im-selector {
+
+}
+</style>
---
+++ src/components/imema/index.js
@@ -0,0 +1,105 @@
+import imSelector from './components/im-selector.vue'
+export default {
+  name: 'im-selector',
+  components: {
+    imSelector
+  },
+  data () {
+    return {
+      id: this.value,
+      mapObj: this.mapModel,
+      mapKey: 'key',
+      mapVal: 'value'
+    }
+  },
+  props: {
+    value: {
+      default: ''
+    },
+    // 容器样式
+    type: {
+      type: String,
+      required: false,
+      default: 'select'
+    },
+    // 组件业务类型
+    dataType: {
+      type: String,
+      required: true
+    },
+    mapModel: {
+      type: Object,
+      default: () => ({})
+    },
+    // 下拉框对象的下拉显示属性-对应的值
+    mapKeyVal: {
+      type: String,
+      required: false
+    }
+  },
+  computed: {
+    // 始终返回渲染组件
+    component () {
+      if (this.type === 'select') return imSelector
+      else {
+        return 'div'
+      }
+    }
+  },
+  render (h) {
+    return h('div', {
+      ref: 'selector',
+      class: 'selector-component'
+    }, [
+      h(this.component, {
+        ref: 'component',
+        props: {
+          dataType: this.dataType
+        },
+        on: {
+          change: e => this.$emit('change', e),
+          input: e => {
+            if (!e.value) {
+              this.mapModel[this.mapKey] = this.mapModel[this.mapVal] = null
+            } else {
+              this.mapModel[this.mapKey] = e.key
+              this.mapModel[this.mapVal] = e.value
+            }
+          }
+        }
+      })
+    ])
+  },
+  methods: {
+
+  },
+  watch: {
+    value: function (val, oldVal) {
+      let obj = {
+        key: this.mapModel[this.mapKey],
+        value: this.mapModel[this.mapVal]
+      }
+      this.$refs.component._setVal(obj)
+    }
+  },
+  created () {
+    if (this.mapKeyVal) {
+      let kv = this.mapKeyVal.split(':')
+      if (kv.length === 2) {
+        this.mapKey = kv[0]
+        this.mapVal = kv[1]
+      } else {
+        this.mapKey = this.mapVal = this.mapKeyVal
+      }
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      let obj = {
+        key: this.mapModel[this.mapKey],
+        value: this.mapModel[this.mapVal]
+      }
+      this.$refs.component._setVal(obj)
+    })
+  }
+}

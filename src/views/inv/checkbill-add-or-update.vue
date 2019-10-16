6c26aa0c672b14cac1e6aef38ded61cd4ce3c704
肖纯凯 <522269416@qq.com>
2019-10-16 16:01:45 +0800

    物料搜索根据仓库过滤

--- src/views/inv/checkbill-add-or-update.vue
+++ src/views/inv/checkbill-add-or-update.vue
@@ -216,7 +216,7 @@ export default {
     prodSeach (queryString, cb) {
       if (queryString) {
         this.$axios
-          .post(this.mixinViewModuleOptions.prodURL, { name: queryString })
+          .post(this.mixinViewModuleOptions.prodURL, { name: queryString,warehouseId:this.dataForm.warehouseId })
           .then(res => {
             for (var i = 0; i < res.length; i++) {
               res[i].value = res[i].val
